ad434be7920a5ad128d319886ef8846f5a6f1a03
Patrick Sturm <siyb@geekosphere.org>
2014-03-20 13:18:03 +0100

    added an interface that describes a WebRequestAdapter

---
+++ library/src/at/diamonddogs/data/adapter/WebRequestAdapter.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2014 Spherical Elephant GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package at.diamonddogs.data.adapter;
+
+import at.diamonddogs.data.dataobjects.WebRequest;
+
+/**
+ * An interface that should be implemented by all {@link WebRequestAdapter},
+ * which handle manipulating {@link WebRequest} parameters in order to provide a
+ * certain functionality.
+ * 
+ * @param <REQUEST>
+ *            the type of {@link WebRequest}
+ */
+public interface WebRequestAdapter<REQUEST extends WebRequest> {
+	public REQUEST getRequest();
+}
