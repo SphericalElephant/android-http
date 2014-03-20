6e8f213c611dcdf351dccaa36cb4244cf04fcda0
Patrick Sturm <siyb@geekosphere.org>
2014-03-20 13:19:33 +0100

    added JSONRequestAdapter, that takes care of building simple JSON requests

---
+++ library/src/at/diamonddogs/data/adapter/json/JSONRequestAdapter.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2014 Spherical Elephant GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package at.diamonddogs.data.adapter.json;
+
+import java.io.UnsupportedEncodingException;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import at.diamonddogs.data.adapter.WebRequestAdapter;
+import at.diamonddogs.data.dataobjects.WebRequest;
+import at.diamonddogs.http.entity.JSONHttpEntity;
+
+public class JSONRequestAdapter implements WebRequestAdapter<WebRequest> {
+	private final WebRequest webRequest;
+
+	public JSONRequestAdapter(WebRequest webRequest, JSONObject input) throws UnsupportedEncodingException {
+		this.webRequest = webRequest;
+		this.webRequest.setHttpEntity(new JSONHttpEntity(input));
+		this.webRequest.addHeaderField("Content-Type", "application/json");
+	}
+
+	public JSONRequestAdapter(WebRequest webRequest, JSONArray input) throws UnsupportedEncodingException {
+		this.webRequest = webRequest;
+		this.webRequest.setHttpEntity(new JSONHttpEntity(input));
+		this.webRequest.addHeaderField("Content-Type", "application/json");
+	}
+
+	@Override
+	public WebRequest getRequest() {
+		return webRequest;
+	}
+}
