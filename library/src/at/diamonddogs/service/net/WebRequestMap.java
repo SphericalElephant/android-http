f232d58db0a8fc03a28ac55f2c725e69951b7845
Patrick Sturm <p.sturm@spherical-elephant.com>
2014-07-28 15:38:07 +0200

    javadoc

--- library/src/at/diamonddogs/service/net/WebRequestMap.java
+++ library/src/at/diamonddogs/service/net/WebRequestMap.java
@@ -11,6 +11,10 @@ import android.support.v4.content.LocalBroadcastManager;
 import at.diamonddogs.data.dataobjects.WebRequest;
 import at.diamonddogs.data.dataobjects.WebRequestFutureContainer;
 
+/**
+ * {@link WebRequestMap} manages {@link WebRequest}s and issues events whenever
+ * {@link WebRequest} are added or removed.
+ */
 public class WebRequestMap {
 	/**
 	 * The action that is used to sinal {@link BroadcastReceiver}s every time a
@@ -25,11 +29,29 @@ public class WebRequestMap {
 	 */
 	public static final String INTENT_EXTRA_WEBREQUEST_COUNT = "at.diamonddogs.service.net.HttpService.INTENT_EXTRA_WEBREQUEST_COUNT";
 
+	/**
+	 * The singleton instance of this {@link WebRequestMap}
+	 */
 	private static WebRequestMap INSTANCE;
 
+	/**
+	 * A {@link Context}
+	 */
 	private Context context;
+
+	/**
+	 * The actual {@link WebRequest} storage
+	 */
 	private Map<String, WebRequestFutureContainer> webRequests;
 
+	/**
+	 * Used to obtain the Singleton instance of {@link WebRequestMap}
+	 * 
+	 * @param c
+	 *            a {@link Context} that is used to obtain the application
+	 *            context
+	 * @return the Singleton instance of {@link WebRequestMap}
+	 */
 	public static final synchronized WebRequestMap getInstance(Context c) {
 		if (INSTANCE == null) {
 			INSTANCE = new WebRequestMap(c);
@@ -43,28 +65,80 @@ public class WebRequestMap {
 		webRequests = Collections.synchronizedMap(new HashMap<String, WebRequestFutureContainer>());
 	}
 
+	/**
+	 * Puts an Object into the map and notifies {@link BroadcastReceiver}s that
+	 * are listening to the {@link WebRequestMap#ACTION_ACTIVE_WEBREQUESTS}
+	 * event.
+	 * 
+	 * @see HashMap#put(Object, Object)
+	 */
 	public WebRequestFutureContainer put(String key, WebRequestFutureContainer value) {
 		sendActiveWebRequestsIntent();
 		return webRequests.put(key, value);
 	}
 
+	/**
+	 * Removes an Object from the map and notifies {@link BroadcastReceiver}s
+	 * that are listening to the {@link WebRequestMap#ACTION_ACTIVE_WEBREQUESTS}
+	 * event.
+	 * 
+	 * @see HashMap#put(Object, Object)
+	 */
 	public WebRequestFutureContainer remove(String key) {
 		sendActiveWebRequestsIntent();
 		return webRequests.remove(key);
 	}
 
+	/**
+	 * Obtains the {@link WebRequestFutureContainer} object known by id
+	 * 
+	 * @param id
+	 *            the id
+	 * @see HashMap#remove(Object)
+	 * @return the {@link WebRequestFutureContainer} object
+	 */
 	public WebRequestFutureContainer getWebRequestFutureContainerById(String id) {
 		return webRequests.get(id);
 	}
 
+	/**
+	 * Checks if the {@link WebRequestFutureContainer} known by id is currently
+	 * managed by {@link WebRequestMap}
+	 * 
+	 * @param id
+	 *            the id to check
+	 * @return <code>true</code> if the {@link WebRequestFutureContainer} is
+	 *         managed, <code>false</code> otherwise
+	 */
 	public boolean hasWebRequestFutureContainerWithId(String id) {
 		return webRequests.containsKey(id);
 	}
 
+	/**
+	 * Cancels the {@link WebRequest} contained in the
+	 * {@link WebRequestFutureContainer} known by id with interruption
+	 * permission
+	 * 
+	 * @param id
+	 *            the id of the {@link WebRequestFutureContainer} to interrupt
+	 * @return <code>true</code> or <code>false</code> depding on the success of
+	 *         the cancel operation
+	 */
 	public boolean cancelWebRequestFutureContainerById(String id) {
 		return cancelWebRequestFutureContainerById(id, true);
 	}
 
+	/**
+	 * Cancels the {@link WebRequest} contained in the
+	 * {@link WebRequestFutureContainer} known by id
+	 * 
+	 * @param id
+	 *            the id of the {@link WebRequestFutureContainer} to interrupt
+	 * @param mayInterruptIfRunning
+	 *            cancel with interrupt?
+	 * @return <code>true</code> or <code>false</code> depding on the success of
+	 *         the cancel operation
+	 */
 	public boolean cancelWebRequestFutureContainerById(String id, boolean mayInterruptIfRunning) {
 		WebRequestFutureContainer container = webRequests.get(id);
 		boolean canceled = container.getFuture().cancel(mayInterruptIfRunning);
@@ -73,7 +147,13 @@ public class WebRequestMap {
 		return canceled;
 	}
 
-	public int size() {
+	/**
+	 * Gets the number of {@link WebRequestFutureContainer}s managed by
+	 * {@link WebRequestMap}
+	 * 
+	 * @return the number of active {@link WebRequestFutureContainer}s
+	 */
+	public int getNumberOfActiveWebRequests() {
 		return webRequests.size();
 	}
 
