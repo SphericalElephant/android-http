5b965da32b905e4265bcce97d9b8db13d246e952
Patrick Sturm <p.sturm@spherical-elephant.com>
2014-08-22 12:22:20 +0200

    added support for user triggered image reloading in ImageLoader

--- library/src/at/diamonddogs/service/processor/ImageLoader.java
+++ library/src/at/diamonddogs/service/processor/ImageLoader.java
@@ -15,14 +15,30 @@
  */
 package at.diamonddogs.service.processor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import android.os.Handler;
+import android.os.Handler.Callback;
+import android.os.Message;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.animation.Animation;
 import android.widget.ImageView;
+import at.diamonddogs.data.dataobjects.WebRequest;
 import at.diamonddogs.service.net.HttpServiceAssister;
+import at.diamonddogs.service.processor.ImageProcessor.ImageProcessHandler;
 
+/**
+ * A utility class that facilitates image loading, image display, default image
+ * display and user retry.
+ */
 public class ImageLoader {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ImageLoader.class.getSimpleName());
+
 	private final HttpServiceAssister assister;
 	private final Handler.Callback callback;
+	private boolean allowUserRetry = true;
 
 	public ImageLoader(HttpServiceAssister assister, Handler.Callback callback) {
 		super();
@@ -30,9 +46,9 @@ public class ImageLoader {
 		this.callback = callback;
 	}
 
-	public void loadImage(ImageView imageView, String url, Animation fadeInAnimation, int defaultImage) {
+	public void loadImage(ImageView imageView, OnClickListener onClickListener, String url, Animation fadeInAnimation, int defaultImage) {
 		ImageProcessor.ImageProcessHandler handler = new ImageProcessor.ImageProcessHandler(imageView, url, fadeInAnimation, defaultImage);
-		handler.setCallback(callback);
+		handler.setCallback(new AllowRetryCallback(imageView, onClickListener, handler));
 		// @formatter:off 
 		assister.runWebRequest(
 			handler,
@@ -42,9 +58,9 @@ public class ImageLoader {
 		// @formatter:on
 	}
 
-	public void loadImage(ImageView imageView, String url, int defaultImage) {
+	public void loadImage(ImageView imageView, OnClickListener onClickListener, String url, int defaultImage) {
 		ImageProcessor.ImageProcessHandler handler = new ImageProcessor.ImageProcessHandler(imageView, url, defaultImage);
-		handler.setCallback(callback);
+		handler.setCallback(new AllowRetryCallback(imageView, onClickListener, handler));
 		// @formatter:off 
 		assister.runWebRequest(
 			handler,
@@ -54,9 +70,9 @@ public class ImageLoader {
 		// @formatter:on
 	}
 
-	public void loadImage(ImageView imageView, String url, Animation fadeInAnimation) {
+	public void loadImage(ImageView imageView, OnClickListener onClickListener, String url, Animation fadeInAnimation) {
 		ImageProcessor.ImageProcessHandler handler = new ImageProcessor.ImageProcessHandler(imageView, url, fadeInAnimation);
-		handler.setCallback(callback);
+		handler.setCallback(new AllowRetryCallback(imageView, onClickListener, handler));
 		// @formatter:off 
 		assister.runWebRequest(
 			handler,
@@ -66,9 +82,9 @@ public class ImageLoader {
 		// @formatter:on
 	}
 
-	public void loadImage(ImageView imageView, String url) {
+	public void loadImage(ImageView imageView, OnClickListener onClickListener, String url) {
 		ImageProcessor.ImageProcessHandler handler = new ImageProcessor.ImageProcessHandler(imageView, url);
-		handler.setCallback(callback);
+		handler.setCallback(new AllowRetryCallback(imageView, onClickListener, handler));
 		// @formatter:off 
 		assister.runWebRequest(
 			handler,
@@ -76,5 +92,53 @@ public class ImageLoader {
 			new ImageProcessor()
 		);
 		// @formatter:on
+	}
+
+	public void setAllowUserRetry(boolean allowUserRetry) {
+		this.allowUserRetry = allowUserRetry;
+	}
+
+	public final class AllowRetryCallback implements Callback {
+		private ImageView imageView;
+		private OnClickListener onClickListener;
+		private ImageProcessHandler handler;
+
+		public AllowRetryCallback(ImageView imageView, OnClickListener onClickListener, ImageProcessHandler handler) {
+			super();
+			this.imageView = imageView;
+			this.onClickListener = onClickListener;
+			this.handler = handler;
+		}
+
+		@Override
+		public boolean handleMessage(Message msg) {
+			if (allowUserRetry && !ServiceProcessorMessageUtil.isSuccessful(msg)) {
+				LOGGER.info("Image download was not successful, allowing the user to retry!");
+				final WebRequest oldWebRequest = ServiceProcessorMessageUtil
+					.getWebRequest(msg);
+				imageView.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						LOGGER.info("The user is retrying the image download");
+
+						// @formatter:off 
+						assister.runWebRequest(
+							handler,
+							ImageProcessor.getDefaultImageRequest(
+								oldWebRequest.getUrl().toString()
+							),
+							new ImageProcessor()
+						);
+						// @formatter:on
+					}
+				});
+				return false;
+			} else {
+				LOGGER.info("Image download succeded, restoring old OnClickListener for image view");
+				imageView.setOnClickListener(onClickListener);
+				return callback.handleMessage(msg);
+			}
+		}
 	}
 }
